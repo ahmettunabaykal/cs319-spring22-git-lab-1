6ca7fae4ebfa51257cef92d11514e39ea09733d6
ahmettunabaykal <72222145+ahmettunabaykal@users.noreply.github.com>
2022-03-04 11:02:26 +0300

    Update Main.java
    add evaluation code in main method

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+	NewCalculator calculator = new NewCalculator();
+
+	// try division
+calculator.Divide(100,12);
+System.out.println(calculator.getLastResult());
+
+//try addition 
+calculator.Add(10,35);
+System.out.println(calculator.getLastResult);
+
+// try subtraction
+calculator.Subtract(50,15);
+System.out.println(calcuator.getLastResult);
 
     }
 }
